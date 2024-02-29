072a0f53d65622a862504d18e395f6e3a5281710
mis-ran <128778285+mis-ran@users.noreply.github.com>
2024-02-29 12:35:22 -0500

    page object model

---
+++ feature/pages/search_page.py
@@ -0,0 +1,22 @@
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class SearchPage:
+    def __init__(self, browser):
+        self.browser = browser
+        self.search_bar_xpath = "//textarea[@name='q']"
+
+    def go_to(self, url):
+        self.browser.get(url)
+
+    def search_for(self, search_word):
+        search_bar = WebDriverWait(self.browser, 10).until(EC.visibility_of_element_located((By.XPATH, self.search_bar_xpath)))
+        assert search_bar.is_displayed(), "The search bar is not visible."
+        search_bar.send_keys(search_word + Keys.ENTER)
+
+    def title_should_contain(self, search_word):
+        WebDriverWait(self.browser, 10).until(EC.title_contains(search_word))
+        assert search_word in self.browser.title, f"The page title does not contain {search_word}"
--- feature/steps/google_search.py
+++ feature/steps/google_search.py
@@ -1,23 +1,19 @@
 from behave import *
-from selenium.webdriver import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
+
+from feature.pages.search_page import SearchPage
 
 
 @given("user visits {url}")
 def step_impl(context, url):
-    context.browser.get(url)
+    context.search_page = SearchPage(context.browser)
+    context.search_page.go_to(url)
 
 
 @then("user searches {search_word}")
 def step_impl(context, search_word):
-    search_bar = WebDriverWait(context.browser, 10).until(EC.visibility_of_element_located((By.XPATH, "//textarea[@name='q']")))
-    assert search_bar.is_displayed(), "The search bar is not visible."
-    search_bar.send_keys(search_word + Keys.ENTER)
+    context.search_page.search_for(search_word)
 
 
 @then("the page title should contain the {search_word}")
 def step_impl(context, search_word):
-    WebDriverWait(context.browser, 10).until(EC.title_contains(search_word))
-    assert search_word in context.browser.title
+    context.search_page.title_should_contain(search_word)
