0e5b3e96e1b004e7c9e992a57a49e7610473b720
mis-ran <128778285+mis-ran@users.noreply.github.com>
2024-02-29 00:15:18 -0500

    chrome_options

--- feature/environment.py
+++ feature/environment.py
@@ -4,7 +4,9 @@ from selenium.webdriver.chrome.options import Options
 
 def before_all(context):
     chrome_options = Options()
-    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("window-size=2560x1440")
+    chrome_options.add_argument("--log-level=3")
     context.browser = webdriver.Chrome(options=chrome_options)
     context.browser.implicitly_wait(10)
 
